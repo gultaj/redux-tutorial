6597b90e95cf1be84386102fe3797fb730954885
gultaj <yaschick.mail@gmail.com>
2016-09-14 14:53:40 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
- class App extends React.Component {
+class App extends React.Component {
     render() {
         return (<div><h1> Webpack + Redux + React </h1></div>);
     }
