198da01071fd0416abaf20559ffa60ce1a8be71d
gultaj <yaschick.mail@gmail.com>
2016-10-06 15:33:42 +0300

    add logging dispatch function

--- src/configureStore.js
+++ src/configureStore.js
@@ -2,10 +2,28 @@ import todoApp from './reducers/';
 import { loadState, saveState } from './utils/localStorage';
 import { createStore } from 'redux';
 
+const addLoggingToDispatch = (store) => {
+    const rawDispatch = store.dispatch;
+    return (action) => {
+       
+        console.group(action.type);
+        console.log('%c prev state', 'color: gray', store.getState());
+        console.log('%c action', 'color: blue', action);
+        const returnValue = rawDispatch(action);
+        console.log('%c next state', 'color: green', store.getState());
+        console.groupEnd(action.type);
+        return returnValue;
+    }
+};
+
 const configureStore = () => {
     const initialState = loadState();
 
     const store = createStore(todoApp, initialState);
+    
+    if (process.env.NODE_ENV !== 'production') { 
+        store.dispatch = addLoggingToDispatch(store);
+    }
 
     store.subscribe(() => {
         saveState({
