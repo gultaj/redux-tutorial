7bc50ca4d3c62778798f6398c2bd8cbec6ecdcb8
gultaj <yaschick.mail@gmail.com>
2016-10-15 16:03:23 +0300

    toggle todo

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -10,6 +10,15 @@ const byId = (state = {}, action) => {
 };
 
 const createList = (filter) => {
+	const handleToggle = (state, action) => {
+		const { result: toggledId, entities } = action.response;
+		const { completed } = entities.todos[toggledId];
+		const shouldUpdate = (
+			(completed && filter === 'active') || (!completed && filter === 'completed')
+		);
+		return shouldUpdate ? state.filter(id => id != toggledId) : state;
+	};
+
 	const ids = (state = [], action) => {
 		switch (action.type) {
 			case 'FETCH_TODOS_SUCCESS': 
@@ -18,6 +27,7 @@ const createList = (filter) => {
 				return filter !== 'completed' ?
 					[...state, action.response.result] :
 					state;
+			case 'TOGGLE_TODO_SUCCESS': return handleToggle(state, action);
 			default: return state;
 		}
 	};
